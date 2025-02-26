4d96c97674ffb5432ee1bcade22aa5023dd98872
Bertil Tandayamo <bertilt4@gmail.com>
2025-02-25 21:31:35 -0500

    Add increment and decrement functionality to SettingsComponent

--- src/components/SettingsComponent.tsx
+++ src/components/SettingsComponent.tsx
@@ -1,33 +1,61 @@
-import { IonAccordion, IonAccordionGroup, IonItem, IonLabel } from '@ionic/react';
+import { IonAccordion, IonAccordionGroup, IonItem, IonLabel, IonButton, IonCol, IonIcon } from '@ionic/react';
+import { addCircleOutline, removeCircleOutline } from 'ionicons/icons';
+import { useState } from 'react';
+
 const SettingsC = () => {
-  return ( 
-        <IonAccordionGroup>
-          <IonAccordion value="first">
-            <IonItem slot="header" color="light">
-              <IonLabel>Bloques</IonLabel>
-            </IonItem>
-            <div className="ion-padding" slot="content">
-              First Content
-            </div>
-          </IonAccordion>
-          <IonAccordion value="second">
-            <IonItem slot="header" color="light">
-              <IonLabel>Naves</IonLabel>
-            </IonItem>
-            <div className="ion-padding" slot="content">
-              Second Content
-            </div>
-          </IonAccordion>
-          <IonAccordion value="third">
-            <IonItem slot="header" color="light">
-              <IonLabel>Camas</IonLabel>
-            </IonItem>
-            <div className="ion-padding" slot="content">
-              Third Content
-            </div>
-          </IonAccordion>
-        </IonAccordionGroup>
-      );
+  const [count, setCount] = useState(1);
+
+  const handleIncrement = () => {
+    if (count < 10) {
+      setCount(count + 1);
     }
- 
+  };
+
+  const handleDecrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <IonAccordionGroup>
+      <IonAccordion value="first">
+        <IonItem slot="header" color="light">
+          <IonLabel>Bloques</IonLabel>
+        </IonItem>
+        <div className="ion-padding" slot="content">
+          <IonItem>
+            <IonLabel>Numero de bloques</IonLabel>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+              <IonButton fill="clear" onClick={handleDecrement} size='large'>
+                <IonIcon slot="icon-only" ios={removeCircleOutline} md={removeCircleOutline}></IonIcon>
+              </IonButton>
+              <IonLabel>{count}</IonLabel>
+              <IonButton size='large'  fill="clear" onClick={handleIncrement}>
+                <IonIcon slot="icon-only" ios={addCircleOutline} md={addCircleOutline}></IonIcon>
+              </IonButton>
+            </div>
+          </IonItem>
+        </div>
+      </IonAccordion>
+      <IonAccordion value="second">
+        <IonItem slot="header" color="light">
+          <IonLabel>Naves</IonLabel>
+        </IonItem>
+        <div className="ion-padding" slot="content">
+          Second Content
+        </div>
+      </IonAccordion>
+      <IonAccordion value="third">
+        <IonItem slot="header" color="light">
+          <IonLabel>Camas</IonLabel>
+        </IonItem>
+        <div className="ion-padding" slot="content">
+          Third Content
+        </div>
+      </IonAccordion>
+    </IonAccordionGroup>
+  );
+}
+
 export default SettingsC;
