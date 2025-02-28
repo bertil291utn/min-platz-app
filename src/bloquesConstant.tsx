fd31d9bcc305136d7448eac8e9ea587d4ce31ec5
Bertil Tandayamo <bertilt4@gmail.com>
2025-02-28 00:31:43 -0500

    Add getSpanishOrdinal function and enhance MonitoreoC for cuadro selection; update UI to display selected bloque details

--- src/bloquesConstant.tsx
+++ src/bloquesConstant.tsx
@@ -2,3 +2,32 @@ export const NUMERO_CAMAS_MIN = 1;
 export const NUMERO_CUADROS_PER_CAMAS_MIN = 1;
 export const NUMERO_MIN = 1;
 export const NUMERO_MAX = 1000;
+
+export const getSpanishOrdinal = (number: number, isLast: boolean = false): string => {
+  if (isLast) return "último";
+
+  const ordinals: { [key: number]: string } = {
+    1: "primero",
+    2: "segundo",
+    3: "tercero",
+    4: "cuarto",
+    5: "quinto",
+    6: "sexto",
+    7: "séptimo",
+    8: "octavo",
+    9: "noveno",
+    10: "décimo",
+    11: "undécimo",
+    12: "duodécimo",
+    13: "decimotercero",
+    14: "decimocuarto",
+    15: "decimoquinto",
+    16: "decimosexto",
+    17: "decimoséptimo",
+    18: "decimoctavo",
+    19: "decimonoveno",
+    20: "vigésimo"
+  };
+
+  return ordinals[number] || `${number}º`;
+};
--- src/components/MonitoreoC.tsx
+++ src/components/MonitoreoC.tsx
@@ -1,10 +1,14 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonIcon, IonItem, IonItemDivider, IonItemGroup, IonLabel, IonList, IonTitle } from '@ionic/react';
-import { addCircle, removeCircle } from 'ionicons/icons';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonGrid, IonIcon, IonItem, IonItemDivider, IonItemGroup, IonLabel, IonList, IonRow, IonTitle } from '@ionic/react';
+import { addCircle, exit, home, removeCircle, returnUpBack, returnUpForward } from 'ionicons/icons';
 import { useState } from 'react';
-import { NUMERO_MAX, NUMERO_MIN } from '../bloquesConstant';
+import { getSpanishOrdinal, NUMERO_MAX, NUMERO_MIN } from '../bloquesConstant';
+import { Bloque, useBloqueInfo } from '../contexts/BloqueInfoContext';
 
 const MonitoreoC = () => {
   const [camaNumber, setCamaNumber] = useState(1);
+  const [cuadroNumber, setCuadroNumber] = useState(1);
+  const [selectedBloque, setSelectedBloque] = useState<Bloque>();
+  const { bloques } = useBloqueInfo()
 
   const handleIncrement = () => () => {
     if (camaNumber < NUMERO_MAX) {
@@ -22,28 +26,83 @@ const MonitoreoC = () => {
     <div>
       <IonTitle>Monitoreo plantas</IonTitle>
       <br />
-      <IonItem>
-        <IonLabel>Ingrese el numero de cama a monitorear</IonLabel>
-        <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-          <IonButton fill="clear" onClick={handleDecrement()} size='large'>
-            <IonIcon slot="icon-only" ios={removeCircle} md={removeCircle}></IonIcon>
-          </IonButton>
-          <IonLabel>{camaNumber}</IonLabel>
-          <IonButton size='large' fill="clear" onClick={handleIncrement()}>
-            <IonIcon slot="icon-only" ios={addCircle} md={addCircle}></IonIcon>
-          </IonButton>
+      <IonItemDivider>
+        <IonGrid>
+          <IonRow>
+            {bloques.map((bloque: Bloque) => (
+              <IonCol size="6" key={bloque.id}>
+                <IonCard onClick={() => setSelectedBloque(bloque)}>
+                  <IonCardHeader>
+                    <IonCardTitle>{bloque.name}</IonCardTitle>
+                    <IonLabel>{bloque.numCamas} camas</IonLabel>
+                  </IonCardHeader>
+                  <IonCardContent>{bloque.description}</IonCardContent>
+                </IonCard>
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+      </IonItemDivider>
+
+      {selectedBloque
+        ?
+        <div>
+          <IonItem>
+            <IonLabel>Ingrese el numero de cama a monitorear</IonLabel>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+              <IonButton fill="clear" onClick={handleDecrement()} size='large'>
+                <IonIcon slot="icon-only" ios={removeCircle} md={removeCircle}></IonIcon>
+              </IonButton>
+              <IonLabel>{camaNumber}</IonLabel>
+              <IonButton size='large' fill="clear" onClick={handleIncrement()}>
+                <IonIcon slot="icon-only" ios={addCircle} md={addCircle}></IonIcon>
+              </IonButton>
+            </div>
+          </IonItem>
+          <br />
+          <IonItem >
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+                <p>Cama #{camaNumber}</p>
+                <h1>Cuadro #{cuadroNumber}</h1>
+              </div>
+              <br />
+
+              <IonGrid>
+                <IonRow>
+                  {[...Array(selectedBloque.numCuadrosPerCama)].map((_, index) => (
+                    <IonCol  key={index + 1}>
+                      <IonButton
+                        fill='outline'
+                        size='large'
+                        onClick={() => setCuadroNumber(index + 1)}
+                      >
+                        {index + 1 <= selectedBloque.numCuadrosPerCama / 2
+                          ? getSpanishOrdinal(index + 1)
+                          : index + 1 === selectedBloque.numCuadrosPerCama ? 'Último'
+                            : getSpanishOrdinal(selectedBloque.numCuadrosPerCama - index)}
+
+                        {index + 1 <= Math.ceil(selectedBloque.numCuadrosPerCama / 2)
+                          ?
+                          // <IonIcon slot="start" ios={home} md={home}></IonIcon>
+                          <p>&nbsp;entrada</p>
+                          :
+                          // <IonIcon slot="start" ios={exit} md={exit}></IonIcon>
+                          <p> &nbsp;salida</p>
+                        }
+                      </IonButton>
+                    </IonCol>
+                  ))}
+                </IonRow>
+              </IonGrid>
+
+            </div>
+          </IonItem>
+
+
         </div>
-      </IonItem>
-<br />
-      <IonItem >
-        <div style={{ display: 'flex', flexDirection: 'column' }}>
-          <p>Cama #{camaNumber}</p>
-          <p>Cuadro #{10}</p>
-        </div>
-      </IonItem>
-
-
-
+        :
+        <p>Seleccione un bloque</p>}
     </div>
 
   );
