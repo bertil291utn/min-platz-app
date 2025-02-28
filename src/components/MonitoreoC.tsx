17b7432d33168e694469a8c51841ebe2972a0c88
Bertil Tandayamo <bertilt4@gmail.com>
2025-02-28 13:26:29 -0500

    Refactor MonitoreoC component: extract cama and cuadro management into SegmentMonitoreoCamas for improved modularity and readability

--- src/components/MonitoreoC.tsx
+++ src/components/MonitoreoC.tsx
@@ -4,24 +4,14 @@ import { useState } from 'react';
 import { NUMERO_MAX, NUMERO_MIN } from '../helpers/bloquesConstant';
 import { Bloque, useBloqueInfo } from '../contexts/BloqueInfoContext';
 import { getSpanishOrdinal } from '../helpers/viewHelper';
+import SegmentMonitoreoCamas from './SegmentMonitoreoCamas';
 
 const MonitoreoC = () => {
-  const [camaNumber, setCamaNumber] = useState(1);
-  const [cuadroNumber, setCuadroNumber] = useState(1);
   const [selectedBloque, setSelectedBloque] = useState<Bloque>();
+
   const { activeBloques } = useBloqueInfo()
 
-  const handleIncrement = () => () => {
-    if (camaNumber < NUMERO_MAX) {
-      setCamaNumber((prev) => prev + 1);
-    }
-  };
-
-  const handleDecrement = () => () => {
-    if (camaNumber > NUMERO_MIN) {
-      setCamaNumber((prev) => prev - 1);
-    }
-  };
+ 
 
   return (
     <div>
@@ -54,53 +44,9 @@ const MonitoreoC = () => {
             ))}
           </IonSegmentContent>
           <IonSegmentContent id="camas">
-            {selectedBloque
-              ?
-              <div>
-                <IonItem>
-                  <IonLabel>Ingrese el numero de cama a monitorear</IonLabel>
-                  <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-                    <IonButton fill="clear" onClick={handleDecrement()} size='large'>
-                      <IonIcon slot="icon-only" ios={removeCircle} md={removeCircle}></IonIcon>
-                    </IonButton>
-                    <IonLabel>{camaNumber}</IonLabel>
-                    <IonButton size='large' fill="clear" onClick={handleIncrement()}>
-                      <IonIcon slot="icon-only" ios={addCircle} md={addCircle}></IonIcon>
-                    </IonButton>
-                  </div>
-                </IonItem>
-                <br />
-                <div style={{ display: 'flex', flexDirection: 'column' }}>
-                  <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
-                    <h1>Cama #{camaNumber}</h1>
-                    <h1>Cuadro #{cuadroNumber}</h1>
-                  </div>
-                  <br />
-
-                  {[...Array(selectedBloque.numCuadrosPerCama)].map((_, index) => (
-                    <IonButton
-                      key={index}
-                      expand='block'
-                      fill='outline'
-                      size='large'
-                      onClick={() => setCuadroNumber(index + 1)}
-                    >
-                      <IonLabel>
-                        {index + 1 <= Math.ceil(selectedBloque.numCuadrosPerCama / 2)
-                          ? `${getSpanishOrdinal(index + 1)} cuadro desde entrada`
-                          : index + 1 === selectedBloque.numCuadrosPerCama
-                            ? 'Último cuadro desde salida'
-                            : `${getSpanishOrdinal(selectedBloque.numCuadrosPerCama - index)} cuadro desde salida`}
-                      </IonLabel>
-                    </IonButton>
-                  ))}
-
-                </div>
-
-
-              </div>
-              :
-              null}
+            <SegmentMonitoreoCamas
+            selectedBloque={selectedBloque}
+            />
           </IonSegmentContent>
           {/* <IonSegmentContent id="third">Third</IonSegmentContent> */}
         </IonSegmentView>
---
+++ src/components/SegmentMonitoreoCamas.tsx
@@ -0,0 +1,86 @@
+import { IonButton, IonIcon, IonItem, IonLabel } from '@ionic/react';
+import { Dispatch, SetStateAction, useState } from 'react';
+import { Bloque } from '../contexts/BloqueInfoContext';
+import { NUMERO_MAX, NUMERO_MIN } from '../helpers/bloquesConstant';
+import { addCircle, removeCircle } from 'ionicons/icons';
+import { getSpanishOrdinal } from '../helpers/viewHelper';
+
+interface SegmentMonitoreoCamasProps {
+  selectedBloque: Bloque | undefined;
+}
+
+const SegmentMonitoreoCamas = ({
+  selectedBloque,
+}: SegmentMonitoreoCamasProps) => {
+
+  const [camaNumber, setCamaNumber] = useState(1);
+  const [cuadroNumber, setCuadroNumber] = useState(1);
+
+
+  const handleIncrement = () => () => {
+    if (camaNumber < NUMERO_MAX) {
+      setCamaNumber((prev) => prev + 1);
+    }
+  };
+
+  const handleDecrement = () => () => {
+    if (camaNumber > NUMERO_MIN) {
+      setCamaNumber((prev) => prev - 1);
+    }
+  };
+
+
+  return (
+    <>
+      {selectedBloque
+        ?
+        <div>
+          <IonItem>
+            <IonLabel>Ingrese el numero de cama a monitorear</IonLabel>
+            <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+              <IonButton fill="clear" onClick={handleDecrement()} size='large'>
+                <IonIcon slot="icon-only" ios={removeCircle} md={removeCircle}></IonIcon>
+              </IonButton>
+              <IonLabel>{camaNumber}</IonLabel>
+              <IonButton size='large' fill="clear" onClick={handleIncrement()}>
+                <IonIcon slot="icon-only" ios={addCircle} md={addCircle}></IonIcon>
+              </IonButton>
+            </div>
+          </IonItem>
+          <br />
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+              <h1>Cama #{camaNumber}</h1>
+              <h1>Cuadro #{cuadroNumber}</h1>
+            </div>
+            <br />
+
+            {[...Array(selectedBloque.numCuadrosPerCama)].map((_, index) => (
+              <IonButton
+                key={index}
+                expand='block'
+                fill='outline'
+                size='large'
+                onClick={() => setCuadroNumber(index + 1)}
+              >
+                <IonLabel>
+                  {index + 1 <= Math.ceil(selectedBloque.numCuadrosPerCama / 2)
+                    ? `${getSpanishOrdinal(index + 1)} cuadro desde entrada`
+                    : index + 1 === selectedBloque.numCuadrosPerCama
+                      ? 'Último cuadro desde salida'
+                      : `${getSpanishOrdinal(selectedBloque.numCuadrosPerCama - index)} cuadro desde salida`}
+                </IonLabel>
+              </IonButton>
+            ))}
+
+          </div>
+
+
+        </div>
+        :
+        null}
+    </>
+  );
+}
+
+export default SegmentMonitoreoCamas;
