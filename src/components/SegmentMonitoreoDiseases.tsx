a41371deedc46cfd5f99175fee36a9a3417da813
Bertil Tandayamo <bertilt4@gmail.com>
2025-03-03 18:35:18 -0500

    Add disease information files and enhance SegmentMonitoreoDiseases with images of acaros

--- src/components/SegmentMonitoreoDiseases.tsx
+++ src/components/SegmentMonitoreoDiseases.tsx
@@ -1,4 +1,7 @@
 import { useMonitoringBloque } from '../contexts/MonitoringBloqueContext';
+import acaros01 from '../assets/rosas-diseases/acaros/acaros01.png';
+import acaros02 from '../assets/rosas-diseases/acaros/acaros02.jpg';
+// import infoAcacros from '../assets/data/rosas-diseases/acaros/info.txt';
 
 const SegmentMonitoreoDiseases = () => {
 
@@ -20,6 +23,8 @@ const SegmentMonitoreoDiseases = () => {
         <h2 style={{ fontSize: '0.875rem', color: '#6B7280' }}>{selectedBloque?.name}</h2>
         <h1 style={{ fontWeight: 'bold' }}>Cama #{selectedCama}</h1>
       </div>
+      <img src={acaros01} alt="Ácaros" style={{ width: '100%', height: '500px' }} />
+      <img src={acaros02} alt="Ácaros2" style={{ width: '100%', height: '500px' }} />
     </div>
 
   );
