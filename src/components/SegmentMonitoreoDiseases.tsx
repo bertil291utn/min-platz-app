bc9341236e46e987dc62c88736743cc5f6a82394
Bertil Tandayamo <bertilt4@gmail.com>
2025-03-04 17:21:13 -0500

    Refactor SegmentMonitoreoDiseases to conditionally render DiseaseCard based on expert user status

--- src/components/SegmentMonitoreoDiseases.tsx
+++ src/components/SegmentMonitoreoDiseases.tsx
@@ -4,11 +4,13 @@ import { DISEASES } from '../helpers/diseases';
 import { IonButton, IonCard, IonCardHeader, IonCardTitle, IonItemDivider } from '@ionic/react';
 import { Disease } from '../interfaces/Diseases';
 import DiseaseImagesModal from './DiseaseImagesModal';
+import { useAuth } from '../contexts/AuthContext';
 
 const SegmentMonitoreoDiseases = () => {
   const [diseasesArr] = useState(DISEASES);
   const { setSelectedDisease, selectedDisease } = useMonitoringBloque();
   const [showModalDisease, setShowModalDisease] = useState<Disease | boolean>(false);
+  const { expertUser } = useAuth();
 
   const handleChangeSegment = (disease: Disease) => () => {
     selectedDisease?.id == disease.id ? setSelectedDisease(undefined) : setSelectedDisease(disease);
@@ -41,31 +43,61 @@ const SegmentMonitoreoDiseases = () => {
       <p>Seleccione una enfermedad</p>
       <br />
       {diseasesArr.map((disease) => (
-        <IonItemDivider key={disease.id}>
-          <div style={{ display: 'flex', flexDirection: 'column', width: '100%', padding: '1rem 0' }}>
-            <IonCard key={disease.id} onClick={handleChangeSegment(disease)}
-              color={selectedDisease?.id == disease.id ? 'primary' : ''}
-            >
-              <IonCardHeader>
-                <IonCardTitle>{disease.name}</IonCardTitle>
-              </IonCardHeader>
-            </IonCard>
-            <IonButton fill="clear" expand='block'
-              onClick={handleViewDisease(disease)}
-            >ver como son {disease.name}?</IonButton>
+        expertUser ? (
+          <div key={disease.id}>
+            <DiseaseCard
+              disease={disease}
+              handleChangeSegment={handleChangeSegment}
+              selectedDisease={selectedDisease}
+            />
           </div>
-        </IonItemDivider>
+        ) : (
+          <IonItemDivider key={disease.id}>
+            <div style={{ display: 'flex', flexDirection: 'column', width: '100%', padding: '1rem 0' }}>
+              <DiseaseCard
+                disease={disease}
+                handleChangeSegment={handleChangeSegment}
+                selectedDisease={selectedDisease}
+              />
+              <IonButton fill="clear" expand='block'
+                onClick={handleViewDisease(disease)}
+              >ver como son {disease.name}?</IonButton>
+            </div>
+          </IonItemDivider>
+        )
       ))}
 
-      {showModalDisease &&
+      {
+        showModalDisease &&
         <DiseaseImagesModal
           isOpen={!!showModalDisease}
           onDismiss={() => setShowModalDisease(false)}
           disease={showModalDisease as Disease}
-        />}
-    </div>
+        />
+      }
+    </div >
 
   );
 }
 
 export default SegmentMonitoreoDiseases;
+
+
+const DiseaseCard = ({ disease, handleChangeSegment, selectedDisease }:
+  {
+    disease: Disease,
+    handleChangeSegment: (disease: Disease) => () => void
+    selectedDisease: Disease | undefined
+
+  }
+) => {
+  return (
+    <IonCard key={disease.id} onClick={handleChangeSegment(disease)}
+      color={selectedDisease?.id == disease.id ? 'primary' : ''}
+    >
+      <IonCardHeader>
+        <IonCardTitle>{disease.name}</IonCardTitle>
+      </IonCardHeader>
+    </IonCard>
+  );
+}
