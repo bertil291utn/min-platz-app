07e88e05d691fc26c6bc1a2c705d4bd99a273ba6
Bertil Tandayamo <bertilt4@gmail.com>
2025-03-04 16:32:30 -0500

    Remove swiper dependency and related code from DiseaseImagesModal component

--- src/components/DiseaseImagesModal.tsx
+++ src/components/DiseaseImagesModal.tsx
@@ -1,12 +1,6 @@
 import { IonButtons, IonButton, IonModal, IonHeader, IonContent, IonToolbar, IonTitle } from '@ionic/react';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import { Disease } from '../interfaces/Diseases';
 
-import acaros01 from '../assets/rosas-diseases/acaros/acaros01.png';
-import acaros02 from '../assets/rosas-diseases/acaros/acaros02.jpg';
-
-import 'swiper/css';
-import '@ionic/react/css/ionic-swiper.css';
 
 
 interface Props {
@@ -29,13 +23,9 @@ const DiseaseImagesModal: React.FC<Props> = ({ isOpen, onDismiss, disease }) => 
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <Swiper>
           {disease.images.map((image, index) => (
-            <SwiperSlide key={index}>
               <img src={`${IMAGES_DISEASE_PATH}/${disease.folderName}/${image.src}`} alt={`${disease.name} ${index + 1}`} style={{ width: '100%', height: 'auto' }} />
-            </SwiperSlide>
           ))}
-        </Swiper>
       </IonContent>
     </IonModal>
   );
